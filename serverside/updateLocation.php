5154f6a53d8acfa42fa6ee9b3e158b6017c9aad7
hashalom <hashalom@c114112.wlan.net.ed.ac.uk>
2016-04-06 13:24:01 +0100

    no change

--- serverside/updateLocation.php
+++ serverside/updateLocation.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
 $ini = parse_ini_file("dbconfig.ini");
 $username = $ini['username'];
 $password = $ini['password'];
@@ -10,31 +11,35 @@ $db = new mysqli($host, $username, $password, $database);
 /*
 remember whenever updating a new user as part of the process first move their location to the archived location and always keep the last location in the table. but in the beginning just inset user locaitons. 
 */
-/*if (isset($_POST['who']) && isset($_POST['containsThis']) && isset($_POST['containsThat']) && isset($_POST['thenTweet'])) {
-    $_POST = array_map("strip_tags", $_POST);
-    $_POST = array_map("trim", $_POST);
+
+//function generating uniqye id
+
+/*
+Generate a unique 24 characters userid
+add it to the users database
+return the value to the user as php response
+the user then will save this in localstorage and uset it.
+*/
+if(isset($_GET['uniqueId'],$_GET['longitude'],$_GET['latitude'],$_GET['accuracy'],$_GET['timeReceived'],$_GET['accuracy'])){
     if ($db->connect_errno > 0) {
         die('Unable to connect to database [' . $db->connect_error . ']');
     } else {
-        $sth = $db->prepare("INSERT INTO `tweetRules` (`who`,`ruleName`, `containsThis`, `containsThat`,`notThis`, `thenTweet`,`catName`) VALUES (?,?,?,?,?,?,?) ");
-        $sth->bind_param('sssssss'
-                , ($_POST['who'])
-                , ($_POST['ruleName'])
-                , ($_POST['containsThis'])
-                , ($_POST['containsThat'])
-                , ($_POST['notThis'])
-                , ($_POST['thenTweet'])
-                , ($_POST['catName']));
-        $OK = $sth->execute();
-        // return if successful or display error
-        if ($OK) {
-            $response = "Success";
-        } else {
-            $response = "Something went wrong.";
-        }
-        echo $response;
+        // $uniqueId = uniqid('', true);
+        // $sth = $db->prepare("INSERT INTO `users` (`uniqueId`) VALUES (?) ");
+        // $sth->bind_param('s'
+        //         , ($uniqueId));
+        // $OK = $sth->execute();
+        // // return if successful or display error
+        // if ($OK) {
+        //     $response = array(
+        //         "status"=>"sucess",
+        //         "uniqueId"=>$uniqueId);
+        // } else {
+            $response = array(
+                "status"=>"failed");
+        // }
+        echo json_encode($response);
         $sth->close();
     }
-}*/
-
+}
 ?>
